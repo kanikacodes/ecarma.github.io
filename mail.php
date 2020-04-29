c0c1efbf92cabb3abfa73d51fbf98b221131ae62
kanika garg <imkanikagarg@gmail.com>
2020-04-29 20:57:39 +0530

    first

---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+
+$EmailTo = "contactus@ecarma.in";
+$Title = "New Message Received";
+
+// prepare email body text
+$Fields .= "Name: ";
+$Fields .= $name;
+$Fields .= "\n";
+
+$Fields.= "Email: ";
+$Fields .= $email;
+$Fields .= "\n";
+
+$Fields.= "Phone Number: ";
+$Fields .= $subject;
+$Fields .= "\n";
+
+$Fields .= "Message: ";
+$Fields .= $message;
+$Fields .= "\n";
+
+
+// send email
+$success = mail($EmailTo,  $Title,  $Fields, "From:".$email);
